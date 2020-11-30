90be043cc5b0a941eed38c6e8c995ce6f8a5b79d
Shaun York <sry@Shauns-MacBook-Pro-2.local>
2020-11-30 13:22:55 -0600

    need awscli

--- init.sh
+++ init.sh
@@ -11,12 +11,19 @@ function install_brew() {
 
 function its_darwin() {
     if ! brew_loc="$(type -p "brew")" || [[ -z $brew_loc ]]; then
-    install_brew()
+    install_brew();
     fi
 
     if ! uuidgen_loc="$(type -p "uuidgen")" || [[ -z $uuidgen_loc ]]; then
     brew install uuidgen
     fi
+
+    if ! awscli_loc="$(type -p "awscli")" || [[ -z $awscli_loc ]]; then
+    brew install awscli
+    fi
 }
 
 
+if 'Darwin' == $(uname); then
+    its_darwin();
+fi
